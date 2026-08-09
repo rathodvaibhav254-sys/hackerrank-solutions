7b16ab964770da2852b842f3284dc14442d53764
Vaibhav Rathod <rathodvaibhav254@gmail.com>
2026-08-09 09:14:56 +0530

    ✅ Input & Output - User Registration System 3 [cpp] — approach1

---
+++ solutions/cpp/medium/input-output-user-registration-system-3-1/solution-approach1.cpp
@@ -0,0 +1,32 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-user-registration-system-3-1/problem?isFullScreen=true
+// Problem     Input & Output - User Registration System 3
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-09, 09:14 a.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string Name;
+    int Age;
+    
+    cin>> Age;
+    cin>> Name;
+    
+    cout<<"User Details:"<<endl;
+    cout<<"Name: "<<Name<<endl;
+    cout<<"Age: "<<Age;
+    
+    return 0;
+}
