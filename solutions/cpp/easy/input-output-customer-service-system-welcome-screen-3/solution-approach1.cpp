40d349743a586b55c8fd22e0229fa1e8b92bb0dd
Vaibhav Rathod <rathodvaibhav254@gmail.com>
2026-08-09 09:14:05 +0530

    ✅ Input & Output - Customer Service System Welcome Screen 3 [cpp] — approach1

---
+++ solutions/cpp/easy/input-output-customer-service-system-welcome-screen-3/solution-approach1.cpp
@@ -0,0 +1,26 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-customer-service-system-welcome-screen-3/problem?isFullScreen=true
+// Problem     Input & Output - Customer Service System Welcome Screen 3
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-09, 09:13 a.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+    string name;
+    cin>> name;
+    cout<<"Welcome"<<" "<<name;
+    return 0;
+}
