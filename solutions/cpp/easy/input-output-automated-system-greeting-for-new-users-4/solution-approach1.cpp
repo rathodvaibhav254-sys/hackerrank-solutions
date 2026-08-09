3855eec01b7ab67b9a5d416727e1f81f2bb0a0e6
Vaibhav Rathod <rathodvaibhav254@gmail.com>
2026-08-09 09:12:55 +0530

    ✅ Input & Output - Automated System Greeting for New Users 4 [cpp] — approach1

---
+++ solutions/cpp/easy/input-output-automated-system-greeting-for-new-users-4/solution-approach1.cpp
@@ -0,0 +1,24 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-automated-system-greeting-for-new-users-4/problem?isFullScreen=true
+// Problem     Input & Output - Automated System Greeting for New Users 4
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-09, 09:12 a.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    cout << "Hello, World!";
+        return 0;
+}
