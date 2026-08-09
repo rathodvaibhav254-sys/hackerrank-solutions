e6051715199ae770c8554fc4a73136fbcff2c7e9
Vaibhav Rathod <rathodvaibhav254@gmail.com>
2026-08-09 09:15:55 +0530

    ✅ Input & Output - Character Analysis Tool 3 [cpp] — approach1

---
+++ solutions/cpp/medium/input-output-character-analysis-tool-3-1/solution-approach1.cpp
@@ -0,0 +1,32 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-character-analysis-tool-3-1/problem?isFullScreen=true
+// Problem     Input & Output - Character Analysis Tool 3
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-09, 09:15 a.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+
+    char ch;
+    
+    cin >> ch;
+
+    int ascii = ch;
+
+    cout << ascii;
+
+    return 0;
+}
