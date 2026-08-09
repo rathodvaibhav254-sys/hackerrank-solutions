0f50bb11e6fb4c8fde5e9c5da080af7ed211f1db
Vaibhav Rathod <rathodvaibhav254@gmail.com>
2026-08-09 09:22:05 +0530

    ✅ Input & Output - ASCII Decoder Tool [cpp] — approach1

---
+++ solutions/cpp/easy/input-output-ascii-decoder-tool-3-1/solution-approach1.cpp
@@ -0,0 +1,28 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-ascii-decoder-tool-3-1/problem?isFullScreen=true
+// Problem     Input & Output - ASCII Decoder Tool
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-09, 09:22 a.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+    int ascii;
+    cin >> ascii;
+
+    cout << (char)ascii;
+
+    return 0;
+}
