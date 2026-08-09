d7823bef65703a27fa8ba3af6be4ef2787f2e03e
Vaibhav Rathod <rathodvaibhav254@gmail.com>
2026-08-09 09:15:26 +0530

    ✅ Input & Output - Gym Membership Registration System 3 [cpp] — approach1

---
+++ solutions/cpp/medium/input-output-gym-membership-registration-system-3/solution-approach1.cpp
@@ -0,0 +1,29 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-gym-membership-registration-system-3/problem?isFullScreen=true
+// Problem     Input & Output - Gym Membership Registration System 3
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-09, 09:15 a.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+string name, membership;
+
+    getline(cin, name);
+    getline(cin, membership);
+
+    cout << "Member Name: " << name << endl;
+    cout << "Selected Membership: " << membership << endl;
+
+}
